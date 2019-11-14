6513c8a87b395c780146ae3ee63cd2368cc46d42
Matteo Baglini <matteo.baglini@gmail.com>
2019-11-14 17:38:27 +0100

    add empty project ready to rock!

---
+++ csharp/gol.tests/TestTheTester.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace gol.tests
+{
+    public class TestTheTester
+    {
+        [Fact]
+        public void ItWorks()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
---
+++ csharp/gol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
