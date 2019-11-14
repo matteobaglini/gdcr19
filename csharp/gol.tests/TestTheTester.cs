1324859e5913be0a38b89b7844f5927b398bb5f7
Matteo Baglini <matteo.baglini@gmail.com>
2019-11-14 17:43:42 +0100

    add deep equal lib to csharp sln

--- csharp/gol.tests/TestTheTester.cs
+++ csharp/gol.tests/TestTheTester.cs
@@ -1,13 +1,28 @@
+using System;
 using Xunit;
+using DeepEqual.Syntax;
 
 namespace gol.tests
 {
     public class TestTheTester
     {
         [Fact]
-        public void ItWorks()
+        public void ClassicWorks()
         {
             Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void DeepWorks()
+        {
+            var foo = new Foo{Bar = "test"};
+            foo.ShouldDeepEqual(new Foo{
+                Bar = "test"
+            });
+        }
+
+        private class Foo{
+            public String Bar { get; set; }
         }
     }
 }
