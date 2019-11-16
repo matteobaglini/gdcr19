98bbfdd4f5af717085f6b623685990a23340c74e
Matteo Baglini <matteo.baglini@gmail.com>
2019-11-16 07:30:59 +0100

    add Unquote assert lib to fsharp

--- fsharp/gol.tests/TestTheTester.fs
+++ fsharp/gol.tests/TestTheTester.fs
@@ -3,10 +3,15 @@ module TestTheTester
 open System
 open Xunit
 open FsCheck.Xunit
+open Swensen.Unquote
 
 [<Fact>]
 let ``It works`` () =
     Assert.Equal(1, 1)
+
+[<Fact>]
+let ``Unquote works`` () =
+    test <@ 1 = 1 @>
 
 [<Property>]
 let ``Reverse of reverse of a list is the original list ``(xs:list<int>) =
